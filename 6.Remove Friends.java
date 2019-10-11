5a67cf412a74014224742e5322f6efcd3aea6dae
Suhas M S <50548519+mssuhas@users.noreply.github.com>
2019-10-11 16:27:31 +0530

    Create 6.Remove Friends.java

---
+++ 6.Remove Friends.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Removefriends {
+
+	public static void main(String[] args) {
+		 Scanner sc= new Scanner(System.in);
+     int N,K,T;
+     int a[]= new int[100];
+     int i,j;
+     T=sc.nextInt();
+     for(int x=0;x<T;x++)
+     {
+	     N=sc.nextInt();
+	     K=sc.nextInt();
+	     
+	     for(i=0;i<N;i++)
+	     {
+	    	 a[i]=sc.nextInt();
+	     }
+	     int M=N;
+	     for(i=0;i<K;i++)
+	     {
+	    	 
+	    	 for(j=0;j<M;j++)
+	    	 {
+	    		 if(a[j]<a[j+1])
+	    		 {
+	    			 for(int k=j;k< M;k++)
+	    			 {
+	    				 a[k]=a[k+1];
+	    			 }
+	    			 M--;
+	    			 break;
+	    		 } 
+	    	 }
+	     }
+	     
+	     for(int k=0;k<M;k++)
+	     {
+	    	 System.out.print(a[k]+" ");
+	    	 
+	     }
+	     System.out.println();
+			
+		}
+ }
+}
